23f3df8b1052601ebf759d2ec6d66434e4116cb8
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-10-22 10:46:06 -0400

    Add bestSport() SQL return statement

--- sql.js
+++ sql.js
@@ -67,7 +67,7 @@ won the most medals, along with the number of medals aliased to 'count'.
 */
 
 const bestSport = country => {
-  return;
+  return `SELECT sport, COUNT(*) AS 'count' FROM GoldMedal WHERE country = '${country}' GROUP BY 1 ORDER BY 2 DESC LIMIT 1;`;
 };
 
 /*
