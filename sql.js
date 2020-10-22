b880135592db3bf4bf52532b34043c2c84850da8
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-10-22 10:34:32 -0400

    Add mostSummerWins() SQL return statement

--- sql.js
+++ sql.js
@@ -31,7 +31,7 @@ won the most summer medals, along with the number of medals aliased to 'count'.
 */
 
 const mostSummerWins = country => {
-  return;
+  return `SELECT year, COUNT(*) AS 'count' FROM GoldMedal WHERE season = 'Summer' AND country = '${country}' GROUP BY 1 ORDER BY 2 DESC LIMIT 1;`;
 };
 
 /*
