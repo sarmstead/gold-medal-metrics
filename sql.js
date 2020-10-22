d63a7cb2c4830c4a8609413d0f1b7888d5997f79
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-10-22 10:38:41 -0400

    Add bestYear() SQL return statement

--- sql.js
+++ sql.js
@@ -49,7 +49,7 @@ won the most medals, along with the number of medals aliased to 'count'.
 */
 
 const bestYear = country => {
-  return;
+  return `SELECT year, COUNT(*) AS 'count' FROM GoldMedal WHERE country = '${country}' GROUP BY 1 ORDER BY 2 DESC LIMIT 1;`;
 };
 
 /*
